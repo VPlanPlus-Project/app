e7b4818077a583e1e7a25e168ceb38b8a5a00373
Julius Vincent Babies <julius@familie-babies.de>
2024-12-21 08:38:44 +0100

    Comment to describe id

--- composeApp/src/commonMain/kotlin/plus/vplan/app/domain/model/Week.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/domain/model/Week.kt
@@ -2,6 +2,9 @@ package plus.vplan.app.domain.model
 
 import kotlinx.datetime.LocalDate
 
+/**
+ * @param id The school id and the week number concatenated with a "/", e.g. "67/13" (13th week of current school year)
+ */
 data class Week(
     val id: String,
     val calendarWeek: Int,
