80e0dff8048445874b131aea4dfce4b853c3d8f1
Julius Babies <julius@familie-babies.de>
2024-12-23 10:04:02 +0100

    Removed everything from home

--- composeApp/src/commonMain/kotlin/plus/vplan/app/feature/home/di/homeModule.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/feature/home/di/homeModule.kt
@@ -1,9 +1,6 @@
 package plus.vplan.app.feature.home.di
 
-import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
-import plus.vplan.app.feature.home.ui.HomeViewModel
 
 val homeModule = module {
-    viewModelOf(::HomeViewModel)
 }
