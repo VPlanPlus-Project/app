9f1a0750a61ea3c22c6c5a7352eb494d088e5b6c
Julius Vincent Babies <julius@familie-babies.de>
2025-01-25 20:39:18 +0100

    Assessment Entities

---
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/domain/model/Assessment.kt
@@ -0,0 +1,19 @@
+package plus.vplan.app.domain.model
+
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+
+data class Assessment(
+    val id: Int,
+    val creator: AppEntity,
+    val createdAt: LocalDateTime,
+    val date: LocalDate,
+    val isPublic: Boolean,
+    val defaultLessonId: String,
+    val description: String,
+    val type: Type,
+) {
+    enum class Type {
+        SHORT_TEST, CLASS_TEST, PROJECT, ORAL, OTHER
+    }
+}
