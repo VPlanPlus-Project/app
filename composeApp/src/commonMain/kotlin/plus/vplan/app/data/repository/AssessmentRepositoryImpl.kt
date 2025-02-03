367c2447ff84569f6dc31c7c202f8d8e56ba6702
Julius Vincent Babies <julius@familie-babies.de>
2025-02-03 21:21:43 +0100

    UseCase

--- composeApp/src/commonMain/kotlin/plus/vplan/app/data/repository/AssessmentRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/data/repository/AssessmentRepositoryImpl.kt
@@ -1,12 +1,21 @@
 package plus.vplan.app.data.repository
 
+import co.touchlab.kermit.Logger
 import io.ktor.client.HttpClient
+import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.http.isSuccess
 import io.ktor.http.parameters
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import plus.vplan.app.api
@@ -14,10 +23,15 @@ import plus.vplan.app.data.source.database.VppDatabase
 import plus.vplan.app.data.source.database.model.database.DbAssessment
 import plus.vplan.app.data.source.network.safeRequest
 import plus.vplan.app.data.source.network.toErrorResponse
+import plus.vplan.app.data.source.network.toResponse
 import plus.vplan.app.domain.data.Response
+import plus.vplan.app.domain.model.AppEntity
 import plus.vplan.app.domain.model.Assessment
 import plus.vplan.app.domain.model.SchoolApiAccess
+import plus.vplan.app.domain.model.VppId
 import plus.vplan.app.domain.repository.AssessmentRepository
+
+private val logger = Logger.withTag("AssessmentRepositoryImpl")
 
 class AssessmentRepositoryImpl(
     private val httpClient: HttpClient,
@@ -61,6 +75,65 @@ class AssessmentRepositoryImpl(
         }
         return Response.Error.Cancelled
     }
+
+    override suspend fun createAssessmentOnline(
+        vppId: VppId.Active,
+        date: LocalDate,
+        type: Assessment.Type,
+        defaultLessonId: Int,
+        isPublic: Boolean,
+        content: String
+    ): Response<Int> {
+        safeRequest(onError = { return it }) {
+            val response = httpClient.post {
+                bearerAuth(vppId.accessToken)
+                contentType(ContentType.Application.Json)
+                url {
+                    protocol = api.protocol
+                    host = api.host
+                    port = api.port
+                    pathSegments = listOf("api", "v2.2", "assessment")
+                }
+                setBody(AssessmentPostRequest(
+                    subjectInstance = defaultLessonId,
+                    date = date.toString(),
+                    isPublic = isPublic,
+                    content = content
+                ))
+            }
+
+            if (response.status != HttpStatusCode.OK) {
+                logger.e { "Error creating assessment: $response" }
+                return response.toResponse()
+            }
+
+            val id = ResponseDataWrapper.fromJson<Int>(response.bodyAsText())
+                ?: return Response.Error.ParsingError(response.bodyAsText())
+
+            return Response.Success(id)
+        }
+        return Response.Error.Cancelled
+    }
+
+    override suspend fun getIdForNewLocalAssessment(): Int {
+        return (vppDatabase.assessmentDao.getSmallestId() ?: -1).coerceAtMost(-1)
+    }
+
+    override suspend fun upsert(assessments: List<Assessment>) {
+        vppDatabase.assessmentDao.upsert(
+            assessments = assessments.map { DbAssessment(
+                id = it.id,
+                createdByProfile = (it.creator as? AppEntity.Profile)?.id,
+                createdBy = (it.creator as? AppEntity.VppId)?.id,
+                createdAt = it.createdAt.toInstant(TimeZone.UTC),
+                date = it.date,
+                isPublic = it.isPublic,
+                defaultLessonId = it.defaultLessonId,
+                description = it.description,
+                type = it.type.ordinal
+            ) }
+        )
+    }
 }
 
 @Serializable
@@ -73,4 +146,12 @@ private data class AssessmentGetResponse(
     @SerialName("type") val type: String,
     @SerialName("created_at") val createdAt: Long,
     @SerialName("created_by") val createdBy: Int,
+)
+
+@Serializable
+private data class AssessmentPostRequest(
+    @SerialName("subject_instance") val subjectInstance: Int,
+    @SerialName("date") val date: String,
+    @SerialName("is_public") val isPublic: Boolean,
+    @SerialName("content") val content: String
 )
--- composeApp/src/commonMain/kotlin/plus/vplan/app/data/source/database/dao/AssessmentDao.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/data/source/database/dao/AssessmentDao.kt
@@ -1,6 +1,8 @@
 package plus.vplan.app.data.source.database.dao
 
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import plus.vplan.app.data.source.database.model.database.DbAssessment
 
@@ -9,4 +11,11 @@ interface AssessmentDao {
 
     @Upsert
     suspend fun upsert(assessment: DbAssessment)
+
+    @Upsert
+    @Transaction
+    suspend fun upsert(assessments: List<DbAssessment>)
+
+    @Query("SELECT MIN(id) FROM assessments WHERE id < 0")
+    suspend fun getSmallestId(): Int?
 }
