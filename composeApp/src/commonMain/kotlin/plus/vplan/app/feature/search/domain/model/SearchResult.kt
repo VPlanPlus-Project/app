7a4ffad435002e5bbf6e21951afdfb012284a025
Julius Vincent Babies <julius@familie-babies.de>
2025-02-07 23:18:00 +0100

    Get substitution plan lessons

--- composeApp/src/commonMain/kotlin/plus/vplan/app/feature/search/domain/model/SearchResult.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/feature/search/domain/model/SearchResult.kt
@@ -1,10 +1,28 @@
 package plus.vplan.app.feature.search.domain.model
 
+import plus.vplan.app.domain.model.Lesson
+
 sealed class SearchResult(val type: Result) {
-    sealed class SchoolEntity(result: Result) : SearchResult(result) {
-        data class Group(val group: plus.vplan.app.domain.model.Group) : SchoolEntity(Result.Group)
-        data class Teacher(val teacher: plus.vplan.app.domain.model.Teacher) : SchoolEntity(Result.Teacher)
-        data class Room(val room: plus.vplan.app.domain.model.Room) : SchoolEntity(Result.Room)
+    sealed class SchoolEntity(
+        result: Result
+    ) : SearchResult(result) {
+
+        abstract val lessons: List<Lesson>
+
+        data class Group(
+            val group: plus.vplan.app.domain.model.Group,
+            override val lessons: List<Lesson>
+        ) : SchoolEntity(Result.Group)
+
+        data class Teacher(
+            val teacher: plus.vplan.app.domain.model.Teacher,
+            override val lessons: List<Lesson>
+        ) : SchoolEntity(Result.Teacher)
+
+        data class Room(
+            val room: plus.vplan.app.domain.model.Room,
+            override val lessons: List<Lesson>
+        ) : SchoolEntity(Result.Room)
     }
 }
 
--- composeApp/src/commonMain/kotlin/plus/vplan/app/feature/search/domain/usecase/SearchUseCase.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/feature/search/domain/usecase/SearchUseCase.kt
@@ -1,23 +1,30 @@
 package plus.vplan.app.feature.search.domain.usecase
 
+import androidx.compose.ui.util.fastFilterNotNull
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.datetime.LocalDate
+import plus.vplan.app.App
+import plus.vplan.app.domain.cache.getFirstValue
 import plus.vplan.app.domain.repository.GroupRepository
 import plus.vplan.app.domain.repository.RoomRepository
+import plus.vplan.app.domain.repository.SubstitutionPlanRepository
 import plus.vplan.app.domain.repository.TeacherRepository
 import plus.vplan.app.domain.usecase.GetCurrentProfileUseCase
-import plus.vplan.app.feature.search.domain.model.Result
 import plus.vplan.app.feature.search.domain.model.SearchResult
+import plus.vplan.app.utils.now
 
 class SearchUseCase(
     private val groupRepository: GroupRepository,
     private val teacherRepository: TeacherRepository,
     private val roomRepository: RoomRepository,
-    private val getCurrentProfileUseCase: GetCurrentProfileUseCase
+    private val getCurrentProfileUseCase: GetCurrentProfileUseCase,
+    private val substitutionPlanRepository: SubstitutionPlanRepository
 ) {
-    operator fun invoke(searchQuery: String) = channelFlow<Map<Result, List<SearchResult>>> {
+    operator fun invoke(searchQuery: String) = channelFlow {
         val query = searchQuery.lowercase()
         val profile = getCurrentProfileUseCase().first()
         val school = profile.getSchoolItem()
@@ -29,16 +36,41 @@ class SearchUseCase(
         ) { groups, teachers, rooms ->
             groups
                 .filter { query in it.name.lowercase() }
-                .map { SearchResult.SchoolEntity.Group(it) }+
+                .map { SearchResult.SchoolEntity.Group(it, emptyList()) } +
                     teachers
                         .filter { query in it.name.lowercase() }
-                        .map { SearchResult.SchoolEntity.Teacher(it) } +
+                        .map { SearchResult.SchoolEntity.Teacher(it, emptyList()) } +
 
                     rooms
                         .filter { query in it.name.lowercase() }
-                        .map { SearchResult.SchoolEntity.Room(it) }
-        }.collectLatest {
-            send(it.groupBy { it.type })
+                        .map { SearchResult.SchoolEntity.Room(it, emptyList()) }
+        }.collectLatest { entityResult ->
+            val send: suspend (results: List<SearchResult>) -> Unit = { send(it.groupBy { entityResult -> entityResult.type }) }
+            send(entityResult)
+            substitutionPlanRepository.getSubstitutionPlanBySchool(school.id, LocalDate.now())
+                .map { lessonIds -> lessonIds.map { lessonId -> App.substitutionPlanSource.getById(lessonId).getFirstValue() }.fastFilterNotNull() }
+                .collectLatest { lessons ->
+                    val result = entityResult.map { schoolEntity ->
+                        when (schoolEntity) {
+                            is SearchResult.SchoolEntity.Group -> {
+                                schoolEntity.copy(
+                                    lessons = lessons.filter { schoolEntity.group.id in it.groups }
+                                )
+                            }
+                            is SearchResult.SchoolEntity.Teacher -> {
+                                schoolEntity.copy(
+                                    lessons = lessons.filter { schoolEntity.teacher.id in it.teachers }
+                                )
+                            }
+                            is SearchResult.SchoolEntity.Room -> {
+                                schoolEntity.copy(
+                                    lessons = lessons.filter { schoolEntity.room.id in it.rooms }
+                                )
+                            }
+                        }
+                    }
+                    send(result)
+                }
         }
     }
 }
