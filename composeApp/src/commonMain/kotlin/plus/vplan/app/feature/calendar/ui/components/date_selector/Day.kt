0d094f5496dba78961140499175d09b039466d81
Julius Vincent Babies <julius@familie-babies.de>
2025-02-17 20:32:38 +0100

    date select styling

--- composeApp/src/commonMain/kotlin/plus/vplan/app/feature/calendar/ui/components/date_selector/Day.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/feature/calendar/ui/components/date_selector/Day.kt
@@ -1,9 +1,10 @@
 package plus.vplan.app.feature.calendar.ui.components.date_selector
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -13,47 +14,60 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.format
 import plus.vplan.app.ui.grayScale
+import plus.vplan.app.ui.thenIf
+import plus.vplan.app.utils.atStartOfWeek
+import plus.vplan.app.utils.shortDayOfWeekNames
+import plus.vplan.app.utils.toDp
 
 @Composable
 fun RowScope.Day(
     date: LocalDate,
-    isSelected: Boolean,
+    selectedDate: LocalDate,
     onClick: () -> Unit = {},
     height: Dp,
-    isOtherMonth: Boolean
+    isOtherMonth: Boolean,
+    scrollProgress: Float
 ) {
+    val isWeekSelected = selectedDate.atStartOfWeek() == date.atStartOfWeek()
     AnimatedContent(
-        targetState = isSelected,
+        targetState = selectedDate == date,
         modifier = Modifier
             .weight(1f)
             .height(height),
     ) { showSelection ->
-        Box(
+        Column (
             modifier = Modifier
                 .fillMaxSize()
                 .clickable { onClick() }
-                .then(
-                    if (showSelection) Modifier.background(MaterialTheme.colorScheme.primaryContainer)
-                    else Modifier
-                )
-                .then(
-                    if (isOtherMonth) Modifier.grayScale()
-                    else Modifier
-                ),
-            contentAlignment = Alignment.Center
+                .thenIf(Modifier.grayScale()) { isOtherMonth },
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(
+                text = date.format(LocalDate.Format { dayOfWeek(shortDayOfWeekNames) }),
+                style = MaterialTheme.typography.labelSmall,
+                color = (if (showSelection) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.outline).copy(alpha = 1-(if (isWeekSelected) scrollProgress.coerceAtMost(1f) else 1f))
+            )
             Text(
                 text = date.dayOfMonth.toString(),
                 color =
                 if (isOtherMonth) Color.Gray
-                else if (showSelection) MaterialTheme.colorScheme.onPrimaryContainer
+                else if (showSelection) MaterialTheme.colorScheme.tertiary
                 else if (date.dayOfWeek in listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)) MaterialTheme.colorScheme.error
-                else MaterialTheme.colorScheme.onSurface
+                else MaterialTheme.colorScheme.onSurface,
+                style = (if (showSelection) MaterialTheme.typography.titleMedium else MaterialTheme.typography.titleSmall).copy(fontWeight = if (showSelection) FontWeight.Black else FontWeight.Bold),
             )
+            Row(
+                modifier = Modifier.height(MaterialTheme.typography.labelSmall.lineHeight.toDp()),
+            ) {
+
+            }
         }
     }
 }
