55b44a2274e0d048df1714f5792912008c00793a
Julius Vincent Babies <julius@familie-babies.de>
2025-03-18 09:59:24 +0100

    NOTICKET version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -121,7 +121,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 5
-        versionName = "0.1-2-internal" // remember to update App.kt
+        versionName = "0.1.2-internal" // remember to update App.kt
     }
     packaging {
         resources {
