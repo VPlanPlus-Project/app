aedb6d4f9e3efcdb4a1e87155e2025b71b887ba1
Julius Vincent Babies <julius@familie-babies.de>
2025-03-22 12:16:10 +0100

    #177 Always reload teachers, rooms and groups on full sync

--- composeApp/src/commonMain/kotlin/plus/vplan/app/domain/repository/RoomRepository.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/domain/repository/RoomRepository.kt
@@ -7,5 +7,5 @@ import plus.vplan.app.domain.model.School
 
 interface RoomRepository: WebEntityRepository<Room> {
     fun getBySchool(schoolId: Int): Flow<List<Room>>
-    suspend fun getBySchoolWithCaching(school: School): Response<Flow<List<Room>>>
+    suspend fun getBySchoolWithCaching(school: School, forceReload: Boolean): Response<Flow<List<Room>>>
 }
