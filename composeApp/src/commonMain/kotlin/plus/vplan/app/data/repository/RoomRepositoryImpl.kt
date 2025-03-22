a8aad5d05e093b03acf801cdc1e7aee7255fc661
Julius Vincent Babies <julius@familie-babies.de>
2025-03-22 12:16:10 +0100

    #177 Always reload teachers, rooms and groups on full sync

--- composeApp/src/commonMain/kotlin/plus/vplan/app/data/repository/RoomRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/data/repository/RoomRepositoryImpl.kt
@@ -37,9 +37,9 @@ class RoomRepositoryImpl(
         return vppDatabase.roomDao.getBySchool(schoolId).map { result -> result.map { it.toModel() } }
     }
 
-    override suspend fun getBySchoolWithCaching(school: School): Response<Flow<List<Room>>> {
+    override suspend fun getBySchoolWithCaching(school: School, forceReload: Boolean): Response<Flow<List<Room>>> {
         val flow = getBySchool(school.id)
-        if (flow.first().isNotEmpty()) return Response.Success(flow)
+        if (flow.first().isNotEmpty() && !forceReload) return Response.Success(flow)
 
         return saveRequest {
             val response = httpClient.get("${api.url}/api/v2.2/school/${school.id}/room") {
