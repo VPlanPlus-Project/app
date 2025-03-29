65feedb2ca8a7cfea5d5cbf215a7d6338f7ae178
Julius Vincent Babies <julius@familie-babies.de>
2025-03-29 18:06:15 +0100

    #185 Better search

---
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/feature/search/domain/usecase/GetSubjectsUseCase.kt
@@ -0,0 +1,15 @@
+package plus.vplan.app.feature.search.domain.usecase
+
+import kotlinx.coroutines.flow.first
+import plus.vplan.app.domain.cache.getFirstValue
+import plus.vplan.app.domain.model.Profile
+import plus.vplan.app.domain.repository.SubjectInstanceRepository
+
+class GetSubjectsUseCase(
+    private val subjectInstanceRepository: SubjectInstanceRepository
+) {
+    suspend operator fun invoke(profile: Profile): List<String> {
+        return (if (profile is Profile.StudentProfile) profile.subjectInstances.first()
+        else subjectInstanceRepository.getBySchool(profile.getSchool().getFirstValue()?.id ?: return emptyList(), false).first()).map { it.subject }.sorted().distinct()
+    }
+}
