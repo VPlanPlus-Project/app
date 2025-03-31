aced92c0ebbdbd08972576a609b097a29294f293
Julius Vincent Babies <julius@familie-babies.de>
2025-03-31 12:54:12 +0200

    #193 added links

--- composeApp/src/commonMain/kotlin/plus/vplan/app/feature/onboarding/stage/a_school_search/ui/OnboardingSchoolSearch.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/feature/onboarding/stage/a_school_search/ui/OnboardingSchoolSearch.kt
@@ -5,9 +5,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import org.koin.compose.viewmodel.koinViewModel
 import plus.vplan.app.feature.onboarding.stage.a_school_search.ui.components.SearchBar
@@ -45,6 +55,24 @@ private fun OnboardingSchoolSearchContent(
             query = state.searchQuery,
             results = state.results,
             onEvent = onEvent
+        )
+        Text(
+            text = buildAnnotatedString {
+                withStyle(MaterialTheme.typography.labelMedium.let { it.copy(lineHeight = it.fontSize) }.toSpanStyle()) {
+                    append("Wenn du fortfährst, akzeptierst du die ")
+                    withLink(LinkAnnotation.Url("https://vplan.plus/privacy", TextLinkStyles(style = SpanStyle(fontWeight = FontWeight.SemiBold, color = MaterialTheme.colorScheme.primary)))) {
+                        append("Datenschutzerklärung")
+                    }
+                    append(" und die ")
+                    withLink(LinkAnnotation.Url("https://vplan.plus/tos", TextLinkStyles(style = SpanStyle(fontWeight = FontWeight.SemiBold, color = MaterialTheme.colorScheme.primary)))) {
+                        append("Nutzungsbedingungen")
+                    }
+                    append(".")
+                }
+            },
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .padding(bottom = 8.dp)
         )
         SearchBar(
             query = state.searchQuery, textFieldError = state.textFieldError, onEvent = onEvent
