371b3298a676af861395d1481ad88f3162758b5f
Julius Vincent Babies <julius@familie-babies.de>
2025-04-04 19:17:32 +0200

    NOTICKET Version v0.1.13-internal

--- composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
@@ -123,8 +123,8 @@ object App {
     lateinit var gradeSource: GradeSource
     lateinit var finalGradeSource: FinalGradeSource
 
-    val versionCode: Int = 1
-    val versionName: String = "0.0.1-alpha" // remember to update build.gradle.kts
+    val versionCode: Int = 30
+    val versionName: String = "0.1.13-internal" // remember to update build.gradle.kts
 }
 
 @Composable
