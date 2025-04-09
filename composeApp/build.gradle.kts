0872c4a00b768ff6afdc2c5caae139184d8ce9d6
Julius Vincent Babies <julius@familie-babies.de>
2025-04-09 21:55:05 +0200

    NOTICKET Version v0.1.18-internal

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -127,8 +127,8 @@ android {
         applicationId = "plus.vplan.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1-alpha" // remember to update App.kt
+        versionCode = 40
+        versionName = "0.1.18-internal" // remember to update App.kt
     }
     packaging {
         resources {
--- composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
@@ -123,8 +123,8 @@ object App {
     lateinit var gradeSource: GradeSource
     lateinit var finalGradeSource: FinalGradeSource
 
-    val versionCode: Int = 1
-    val versionName: String = "0.0.1-alpha" // remember to update build.gradle.kts
+    val versionCode: Int = 40
+    val versionName: String = "0.1.18-internal" // remember to update build.gradle.kts
 }
 
 @Composable
