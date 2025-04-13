e9a49ef4df88e38415f519c77dfcedb95ac302ec
Julius Vincent Babies <julius@familie-babies.de>
2025-04-13 22:33:39 +0200

    NOTICKET Version v0.1.21-closed

--- composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
@@ -123,8 +123,8 @@ object App {
     lateinit var gradeSource: GradeSource
     lateinit var finalGradeSource: FinalGradeSource
 
-    val versionCode: Int = 1
-    val versionName: String = "0.0.1-alpha" // remember to update build.gradle.kts
+    val versionCode: Int = 45
+    val versionName: String = "0.1.21-closed" // remember to update build.gradle.kts
 }
 
 @Composable
