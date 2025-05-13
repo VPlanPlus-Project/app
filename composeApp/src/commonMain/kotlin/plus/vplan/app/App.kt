09121b2c6840bbad5edb15d39bc0f4daf519a900
Julius Vincent Babies <julius@familie-babies.de>
2025-05-13 18:14:40 +0200

    NOTICKET v0.1.29-internal

--- composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
@@ -125,8 +125,8 @@ object App {
     lateinit var gradeSource: GradeSource
     lateinit var finalGradeSource: FinalGradeSource
 
-    const val VERSION_CODE: Int = 1
-    const val VERSION_NAME: String = "0.0.1-alpha" // remember to update build.gradle.kts
+    const val VERSION_CODE: Int = 61
+    const val VERSION_NAME: String = "0.1.29-internal" // remember to update build.gradle.kts
 }
 
 @Composable
