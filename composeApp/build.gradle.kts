6c9289199cbdc9c9e5a2033bf8cdf5841df34abb
Julius Vincent Babies <julius@familie-babies.de>
2025-05-15 21:51:19 +0200

    NOTICKET Version v0.1.34-closed

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -133,8 +133,8 @@ android {
         applicationId = "plus.vplan.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1-alpha" // remember to update App.kt
+        versionCode = 66
+        versionName = "0.1.34-closed" // remember to update App.kt
     }
     packaging {
         resources {
--- composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/App.kt
@@ -124,8 +124,8 @@ object App {
     lateinit var gradeSource: GradeSource
     lateinit var finalGradeSource: FinalGradeSource
 
-    const val VERSION_CODE: Int = 1
-    const val VERSION_NAME: String = "0.0.1-alpha" // remember to update build.gradle.kts
+    const val VERSION_CODE: Int = 66
+    const val VERSION_NAME: String = "0.1.34-closed" // remember to update build.gradle.kts
 }
 
 @Composable
