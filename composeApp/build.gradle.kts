70c67ef2fc59e77dd4a607fa83b5061fbd368073
Julius Vincent Babies <julius@familie-babies.de>
2025-05-15 21:52:26 +0200

    NOTICKET Version v0.1.34-internal

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -133,8 +133,8 @@ android {
         applicationId = "plus.vplan.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1-alpha" // remember to update App.kt
+        versionCode = 67
+        versionName = "0.1.34-internal" // remember to update App.kt
     }
     packaging {
         resources {
