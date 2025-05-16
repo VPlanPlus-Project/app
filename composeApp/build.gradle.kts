1fb8d5de57f73ff1ce9f3d95c536b0a84ecbdd8d
Julius Vincent Babies <julius@familie-babies.de>
2025-05-16 08:36:54 +0200

    NOTICKET Version v0.1.36-closed

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -133,8 +133,8 @@ android {
         applicationId = "plus.vplan.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1-alpha" // remember to update App.kt
+        versionCode = 68
+        versionName = "0.1.36-closed" // remember to update App.kt
     }
     packaging {
         resources {
