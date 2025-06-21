41606782a3a86a25688c0ef059d13860d6b2e546
Julius Vincent Babies <julius@familie-babies.de>
2025-06-21 21:43:45 +0200

    NOTICKET Version v0.1.48-prod

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,8 +5,8 @@ import java.util.Base64
 import kotlin.text.Charsets
 
 object ApplicationConfig {
-    const val APP_VERSION_NAME = "0.0.1-alpha"
-    const val APP_VERSION_CODE = 1
+    const val APP_VERSION_NAME = "0.1.48-prod"
+    const val APP_VERSION_CODE = 89
     var isDebug = false
 }
 
