29c5ed8fbf7c203384ef3dc75e2149805f1f8daa
Julius Vincent Babies <julius@familie-babies.de>
2025-06-27 22:50:55 +0200

    NOTICKET iOS Compatibility

--- composeApp/src/iosMain/kotlin/plus/vplan/app/SyncHelper.kt
+++ composeApp/src/iosMain/kotlin/plus/vplan/app/SyncHelper.kt
@@ -1,10 +1,10 @@
 package plus.vplan.app
 
-import plus.vplan.app.feature.sync.domain.usecase.fullsync.FullSyncCause
+import plus.vplan.app.feature.sync.domain.usecase.FullSyncCause
 import plus.vplan.app.feature.sync.domain.usecase.fullsync.FullSyncUseCase
 
 @Suppress("unused") // Called in Swift App
 fun sync() {
     val useCase: FullSyncUseCase = getKoinInstance()
-    useCase.invoke(FullSyncCause.Manual)
+    useCase.invoke(FullSyncCause.Job)
 }
