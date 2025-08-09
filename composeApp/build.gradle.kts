e986a3c7033747ad8db9969bc768d367d6dbc1e9
Julius Vincent Babies <julius@familie-babies.de>
2025-08-09 20:13:55 +0300

    NOTICKET Version v0.1.57-internal

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,8 +5,8 @@ import java.util.Base64
 import kotlin.text.Charsets
 
 object ApplicationConfig {
-    const val APP_VERSION_NAME = "0.1.57-closed"
-    const val APP_VERSION_CODE = 104
+    const val APP_VERSION_NAME = "0.1.57-internal"
+    const val APP_VERSION_CODE = 105
     var isDebug = false
 }
 
