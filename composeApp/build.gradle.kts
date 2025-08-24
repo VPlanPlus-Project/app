c8209cc5e54c059557db22a2b70b37484fb23f7c
Julius Vincent Babies <julius@familie-babies.de>
2025-08-24 11:37:12 +0200

    NOTICKET Version v0.2.3-closed

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,8 +5,8 @@ import java.util.Base64
 import java.util.Properties
 
 object ApplicationConfig {
-    const val APP_VERSION_NAME = "0.2.3-internal"
-    const val APP_VERSION_CODE = 127
+    const val APP_VERSION_NAME = "0.2.3-closed"
+    const val APP_VERSION_CODE = 126
 }
 
 val localProperties = Properties().apply {
