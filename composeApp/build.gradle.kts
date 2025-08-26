810748165af9e9d908c7ab49960f3d2b07148550
Julius Vincent Babies <julius@familie-babies.de>
2025-08-26 19:47:47 +0200

    NOTICKET Version v0.2.4-production

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,8 +5,8 @@ import java.util.Base64
 import java.util.Properties
 
 object ApplicationConfig {
-    const val APP_VERSION_NAME = "0.1.63-production"
-    const val APP_VERSION_CODE = 123
+    const val APP_VERSION_NAME = "0.2.4-production"
+    const val APP_VERSION_CODE = 128
 }
 
 val localProperties = Properties().apply {
