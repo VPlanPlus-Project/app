7b90a52beec75ee7ba0e51dcbd4ba44ce711c4d0
Julius Vincent Babies <julius@familie-babies.de>
2025-09-04 09:16:14 +0200

    NOTICKET Version 0.2.6-internal

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,8 +5,8 @@ import java.util.Base64
 import java.util.Properties
 
 object ApplicationConfig {
-    const val APP_VERSION_NAME = "0.2.5-production"
-    const val APP_VERSION_CODE = 131
+    const val APP_VERSION_NAME = "0.2.6-internal"
+    const val APP_VERSION_CODE = 135
 }
 
 val localProperties = Properties().apply {
