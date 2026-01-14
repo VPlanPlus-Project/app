464e162108008b0a2cc8ee5b562bbf5e918f3c6a
Julius Babies <julius@familie-babies.de>
2026-01-14 14:14:17 +0100

    NOTICKET Version fix

--- composeApp/src/appleMain/kotlin/plus/vplan/app/App.apple.kt
+++ composeApp/src/appleMain/kotlin/plus/vplan/app/App.apple.kt
@@ -3,17 +3,17 @@ package plus.vplan.app
 import platform.Foundation.NSBundle
 
 actual val versionCode: Int = NSBundle.mainBundle
-    .objectForInfoDictionaryKey("CFBundleShortVersionString")
+    .objectForInfoDictionaryKey("CFBundleVersion")
     ?.toString()
     ?.also {
         require(it.toIntOrNull() != null) {
-            "CFBundleShortVersionString should be Integer, was $it"
+            "CFBundleVersion should be Integer, was $it"
         }
     }
     ?.toInt()
     ?: -1
 
 actual val versionName: String = NSBundle.mainBundle
-    .objectForInfoDictionaryKey("CFBundleVersion")
+    .objectForInfoDictionaryKey("CFBundleShortVersionString")
     ?.toString()
     ?: "unknown"
