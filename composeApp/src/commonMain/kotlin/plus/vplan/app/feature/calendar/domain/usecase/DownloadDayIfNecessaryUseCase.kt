0783d855b8fd2652fe3ca39a32cd9d47d83e4baf
Julius Vincent Babies <julius@familie-babies.de>
2026-03-25 20:16:50 +0100

    #487 Only show progress bar when actually fetching data

--- composeApp/src/commonMain/kotlin/plus/vplan/app/feature/calendar/domain/usecase/DownloadDayIfNecessaryUseCase.kt
+++ composeApp/src/commonMain/kotlin/plus/vplan/app/feature/calendar/domain/usecase/DownloadDayIfNecessaryUseCase.kt
@@ -4,32 +4,39 @@ package plus.vplan.app.feature.calendar.domain.usecase
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import kotlinx.datetime.LocalDate
 import plus.vplan.app.core.data.timetable.TimetableRepository
 import plus.vplan.app.core.model.School
 import plus.vplan.app.core.model.Timetable
 import plus.vplan.app.core.sync.domain.usecase.sp24.UpdateTimetableUseCase
-import plus.vplan.app.core.utils.coroutines.ObservableMutex
 import plus.vplan.app.core.utils.date.now
-import kotlin.time.Duration.Companion.milliseconds
 
 class DownloadDayIfNecessaryUseCase(
     private val updateTimetableUseCase: UpdateTimetableUseCase,
     private val timetableRepository: TimetableRepository,
 ) {
 
-    private val mutex = ObservableMutex()
-    val isRunning = mutex.isLocked.debounce(50.milliseconds)
+    private val mutex = Mutex()
+    val isRunning: StateFlow<Boolean>
+        field = MutableStateFlow(false)
     suspend operator fun invoke(date: LocalDate, school: School.AppSchool) = withContext(Dispatchers.Default) {
         if (date > LocalDate.now()) return@withContext
         mutex.withLock {
             val timetables = timetableRepository.getTimetables(school).first()
                 .filter { it.week.start <= date }
             if (timetables.isEmpty() || timetables.maxBy { it.week.start }.dataState == Timetable.HasData.Unknown) {
-                updateTimetableUseCase.updateTimetableRelatedToDate(date, school)
+                try {
+                    isRunning.value = true
+                    updateTimetableUseCase.updateTimetableRelatedToDate(date, school)
+                } finally {
+                    isRunning.value = false
+                }
             }
         }
     }
