c040468a2aa14aaffc1832db076420f34d8af6b8
Julius Vincent Babies <julius@familie-babies.de>
2026-03-26 13:45:45 +0100

    NOTICKET Version v0.4.16.7-open

--- build-logic/convention/src/main/kotlin/ApplicationConfig.kt
+++ build-logic/convention/src/main/kotlin/ApplicationConfig.kt
@@ -4,9 +4,9 @@ import org.gradle.api.Project
 val applicationConfig = ApplicationConfig(
     versionMajor = 0,
     versionMinor = 4,
-    versionPatch = 6,
-    build = 1,
-    channel = ApplicationConfig.Channel.Internal,
+    versionPatch = 16,
+    build = 7,
+    channel = ApplicationConfig.Channel.Open,
     android = ApplicationConfig.Android(
         minSdk = 24,
         targetSdk = 36
